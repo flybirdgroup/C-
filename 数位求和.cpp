f4d271626c6f2d505ea55b8814eb8bb05b40b8a5
flybirdgroup <53985730+flybirdgroup@users.noreply.github.com>
2024-08-09 19:14:02 +0800

    updated

---
+++ 数位求和.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+int c,e,m;
+cin >> c >> e >> m;
+int sum;
+sum = c + e + m;
+if (sum > 270) {
+  cout << "Yes";     
+}
+else {
+cout << "No";
+} 
+return 0;
+}
+
+
